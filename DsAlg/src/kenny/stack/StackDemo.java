e5c765aab94785f23db2ee5e87f25662993353e8
KennyChenFight <kennychen851228@gmail.com>
2018-06-24 21:46:53 +0800

    update stack using array to method

--- DsAlg/src/kenny/stack/StackDemo.java
+++ DsAlg/src/kenny/stack/StackDemo.java
@@ -5,23 +5,34 @@ import java.util.Random;
 public class StackDemo {
 
     public static void main(String[] args) {
+        stackUsingArrayDemo();
+    }
+
+    /*
+    Stack using array simple demo
+    first push item to full
+    second pop per step
+    third print stack
+     */
+    public static void stackUsingArrayDemo() {
         StackUsingArray stack = StackOperation.createStackUsingArray(5);
 
         // push item to stack until stack is full
-        System.out.println("push item to stack:");
+        System.out.println("first push item to stack:");
         for (int i = 0; i < 5; i++) {
             Random random = new Random();
             int number = random.nextInt(5);
             System.out.print(number + " ");
             StackOperation.push(stack, number);
         }
-        System.out.println();
+        System.out.print("\n");
+        System.out.println("Stack is Full:" + StackOperation.isFull(stack));
 
         // print stack item
         StackOperation.printStack(stack);
         System.out.print("\n");
 
-        System.out.println("stack pop per step:");
+        System.out.println("then stack pop per step:");
         // pop stack item and print stack item
         for (int i = 0; i < 5; i++) {
             StackOperation.pop(stack);
